ba7d6fa87aaa01093b70be6c25a4a433f03ad798
Pasindu-Lakmal <pasindu1llak@gmail.com>
2022-08-19 00:05:05 +0530

    muiTypograpyTypes

---
+++ src/components/MuiTypography.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { Typography } from "@mui/material";
+
+export const MuiTypography = () => {
+  return (
+    <div>
+      <Typography variant="h1">h1 Heading</Typography>
+      <Typography variant="h2">h2 Heading</Typography>
+      <Typography variant="h3">h3 Heading</Typography>
+      <Typography variant="h4">h4 Heading</Typography>
+      <Typography variant="h5">h5 Heading</Typography>
+      <Typography variant="h6">h6 Heading</Typography>
+
+      <Typography variant="subtitle1">Sub Title 1</Typography>
+      <Typography variant="subtitle2">Sub Title 2</Typography>
+
+      <Typography variant="body1">
+        Body 1 = Lorem ipsum dolor, sit amet consectetur adipisicing elit. Odio
+        reprehenderit aliquam doloremque impedit, error dolorem nemo sunt
+        molestias! Vero, explicabo.
+      </Typography>
+      <Typography variant="body2">
+        Body 2 = Lorem ipsum dolor, sit amet consectetur adipisicing elit. Odio
+        reprehenderit aliquam doloremque impedit, error dolorem nemo sunt
+        molestias! Vero, explicabo.
+      </Typography>
+    </div>
+  );
+};
